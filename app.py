80610831bc217eaee767a61794e8018859ac591e
Basma Ebeid <bebeid@ualberta.ca>
2023-11-18 15:47:58 -0700

    hi

--- app.py
+++ app.py
@@ -14,10 +14,9 @@ def submit():
     # Redirect to the workout page with user name as a query parameter
     return redirect(url_for('workout', user_name=user_name))
 
-@app.route('/workout')
-def workout():
-    # Render the workout.html template
-    return render_template('workout.html')
+@app.route('/workout/<user_name>')
+def workout(user_name):
+    return render_template('workout.html', user_name=user_name)
 
 @app.route('/process_workout', methods=['POST'])
 def process_workout():
@@ -26,7 +25,26 @@ def process_workout():
 
     # Process the workout duration as needed
 
-    return f"Hello {user_name}! You selected a {duration} workout duration."
+    # Redirect to the next section based on the chosen duration
+    return redirect(url_for('extended_workout', user_name=user_name))
+
+@app.route('/extended_workout/<user_name>')
+def extended_workout(user_name):
+    return render_template('workout_type.html', user_name=user_name)
+
+@app.route('/choose_exercises', methods=['POST'])
+def choose_exercises():
+    user_name = request.form.get('user_name')
+    category = request.form.get('category')
+    selected_exercises = request.form.getlist('exercises')
+
+    # Process the selected exercises as needed
+    # For demonstration, I'm just printing the values here
+    print(f"User: {user_name}, Category: {category}, Exercises: {', '.join(selected_exercises)}")
+
+    # You can save these values to local variables or a database if needed
+
+    return f"Hello {user_name}! You selected {', '.join(selected_exercises)} for {category} workout."
 
 if __name__ == '__main__':
     app.run(debug=True)
