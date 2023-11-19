d209c1d6aa42d594dac282e30aa1660efea5b5ee
Basma Ebeid <bebeid@ualberta.ca>
2023-11-18 18:31:34 -0700

    first attempt

--- app.py
+++ app.py
@@ -55,6 +55,126 @@ from flask import Flask, render_template, request, redirect, url_for
 import random
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Set a secret key for the session, replace with a secure key in production
+
+def workout_generator(duration):
+
+    if duration == "30-45 mins":
+        n = 6    #n is the number of workouts
+    if duration == "45-60 mins":
+        n = 8
+    if duration == "60-75 mins":
+        n = 10
+    if duration == "75-90 mins":
+        n = 12
+
+    chest = [
+        "Barbell Bench Press",
+        "Dumbbell Bench Press",
+        "Incline Bench Press",
+        "Decline Bench Press",
+        "Push-Ups",
+        "Chest Dips",
+        "Chest Flyes",
+        "Cable Crossover",
+        "Pec Deck Machine",
+        "Medicine Ball Push-Ups",
+        "Landmine Press",
+        "Machine Chest Press",
+        "Plyometric Push-Ups",
+        "Single-Arm Dumbbell Press"
+    ]
+    back = [
+        "Pull-Ups",
+        "Deadlifts",
+        "Bent-Over Rows",
+        "Lat Pulldowns",
+        "T-Bar Rows",
+        "Seated Cable Rows",
+        "Chin-Ups",
+        "One-Arm Dumbbell Rows",
+        "Hyperextensions",
+        "Good Mornings",
+        "Reverse Flyes",
+        "Pullovers",
+        "Shrugs",
+        "Rack Pulls"
+    ]
+    arm = [
+        "Bicep Curls",
+        "Tricep Dips",
+        "Hammer Curls",
+        "Tricep Extensions",
+        "Preacher Curls",
+        "Skull Crushers",
+        "Concentration Curls",
+        "Tricep Kickbacks",
+        "Reverse Curls",
+        "Overhead Tricep Press",
+        "Zottman Curls",
+        "Close-Grip Bench Press",
+        "Barbell Curls",
+        "Rope Pushdowns"
+    ]
+    core = [
+        "Plank",
+        "Russian Twists",
+        "Crunches",
+        "Leg Raises",
+        "Mountain Climbers",
+        "Bicycle Crunches",
+        "Hanging Leg Raises",
+        "Woodchoppers",
+        "Dead Bug Exercise",
+        "Ab Rollouts",
+        "Side Plank",
+        "Flutter Kicks",
+        "Hollow Body Hold",
+        "Medicine Ball Slams"
+    ]
+
+    leg = [
+        "Squats",
+        "Lunges",
+        "Deadlifts",
+        "Leg Press",
+        "Leg Curls",
+        "Calf Raises",
+        "Step-Ups",
+        "Romanian Deadlifts",
+        "Box Jumps",
+        "Glute Bridges",
+        "Hack Squats",
+        "Bulgarian Split Squats",
+        "Walking Lunges",
+        "Seated Calf Raises"
+    ]
+
+    glute = [
+        "Hip Thrusts",
+        "Lunges",
+        "Deadlifts",
+        "Glute Bridges",
+        "Donkey Kicks",
+        "Fire Hydrants",
+        "Sumo Squats",
+        "Bulgarian Split Squats",
+        "Clamshells",
+        "Step-Ups",
+        "Cable Kickbacks",
+        "Single-Leg Glute Bridges",
+        "Barbell Hip Thrusts",
+        "Resistance Band Walks"
+    ]
+    workPerGroup = round(n/len(muscle_group))
+    workout_List = []
+    for word in muscle_group:
+        workout_List += random.sample(word,workPerGroup)
+    return workout_List
+
+def muscle_group(selected_exercises):
+    select_group_list = list(selected_exercises)
+    return select_group_list
 
 @app.route('/')
 def home():
@@ -72,6 +192,7 @@ def choose_exercises():
     category = request.form.get('category')
     selected_exercises = request.form.getlist('exercises')
 
+    muscle_group(selected_exercises)
     # Process the selected exercises as needed
     # For demonstration, I'm just printing the values here
     print(f"User: {user_name}, Category: {category}, Exercises: {', '.join(selected_exercises)}")
@@ -83,28 +204,27 @@ def choose_exercises():
     elif category == 'lower_body':
         return render_template('lower_body_exercises.html', user_name=user_name)
     elif category == 'full_body':
-        return render_template('workout.html', user_name=user_name)
+        return render_template('full_body.html', user_name=user_name)
 
 
 @app.route('/process_workout', methods=['POST'])
 def process_workout():
     user_name = request.form.get('user_name')
     duration = request.form.get('duration')
-
+    workout_List = workout_generator(duration)
+    
     # Process the workout duration as needed
     # For demonstration, I'm just printing the values here
     print(f"User: {user_name}, Duration: {duration}")
-    if duration == "30-45 mins":
-        n = 5   #n is the number of workouts
-    if duration == "45-60 mins":
-        n = 8
-    if duration == "60-75 mins":
-        n = 11
-    if duration == "75-90 mins":
-        n = 14
     
-    return f"Hello {user_name}! You selected a {duration} workout duration."
+    return redirect(url_for('display_words', words=workout_List))
 
+@app.route('/display_words')
+def display_words():
+    # Pass the word_list to the template
+    workout_List = request.args.get('words', '').split(',')
+
+    return render_template('workout_display.html', words=workout_List)
 
 if __name__ == '__main__':
     app.run(debug=True)
